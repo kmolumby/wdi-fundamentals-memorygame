64a5b81c78f1c429a2bcb3ad75daa5f52df27e5c
Kelly Molumby <kellymolumby@Kellys-iMac.local>
2018-09-02 11:02:00 -0600

    created game logic
    pwd
    git status

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,31 @@
 
-var cards = ['queen','queen','king','king'];
+var cards = [
+
+	{
+		rank : "queen",
+		suit : "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+
+	{
+		rank : "queen",
+		suit : "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+
+	{
+		rank : "king",
+		suit : "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+
+	{
+		rank : "king",
+		suit : "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	}
+
+];
 
 var cardsInPlay = [];
 
@@ -12,17 +38,20 @@ var checkForMatch = function() {
 			alert("sorry try again");
 	};
 
+
 }
 
 var flipCard = function(cardId) {
-	console.log("User flipped "+ cards[cardId]);
+	console.log("User flipped "+ cards[cardId].rank);
 
-	cardsInPlay.push(cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 
 			if (cardsInPlay.length === 2) {
 
 	checkForMatch();	
-	
+
 	};
 
 };
