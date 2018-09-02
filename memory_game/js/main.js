ff48308a27cf2b09650ed78d7791fa6fd21b5208
Kelly Molumby <kellymolumby@Kellys-iMac.local>
2018-09-02 08:07:44 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,27 +1,33 @@
+
 var cards = ['queen','queen','king','king'];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
+var checkForMatch = function() {
 
-cardsInPlay.push(cardOne);
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");	
 
-console.log("User flipped queen");
+		} else {
+			alert("sorry try again");
+	};
 
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped king")
-
-
-if (cardsInPlay.length === 2) {
-
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");	
-
-} else {
-	alert("sorry try again");
 }
+
+var flipCard = function(cardId) {
+	console.log("User flipped "+ cards[cardId]);
+
+	cardsInPlay.push(cards[cardId]);
+
+			if (cardsInPlay.length === 2) {
+
+	checkForMatch();	
 	
-}
+	};
+
+};
+
+flipCard(0);
+flipCard(2);
+
+
