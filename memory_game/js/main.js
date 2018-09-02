a7ed1cdd06a0a9aa4e16ec941effa39c35bfe3e8
Kelly Molumby <kellymolumby@Kellys-iMac.local>
2018-09-02 13:15:22 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,4 @@
-
+// Card Objects
 var cards = [
 
 	{
@@ -27,7 +27,13 @@ var cards = [
 
 ];
 
+
+
+
 var cardsInPlay = [];
+
+
+//Match function 
 
 var checkForMatch = function() {
 
@@ -41,12 +47,18 @@ var checkForMatch = function() {
 
 }
 
-var flipCard = function(cardId) {
+//Flip Card function 
+
+var flipCard = function() {
+
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped "+ cards[cardId].rank);
 
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
+
+	this.setAttribute('src', cards[cardId].cardImage);
 
 			if (cardsInPlay.length === 2) {
 
@@ -56,7 +68,28 @@ var flipCard = function(cardId) {
 
 };
 
-flipCard(0);
-flipCard(2);
 
+//Create Board function 
+
+var createBoard = function () {
+
+  for (var i = 0; i < cards.length; i++) {
+
+  	var cardElement = document.createElement('img');
+
+  	cardElement.setAttribute('src', 'images/back.png');
+
+  	cardElement.setAttribute('data-id', i);
+
+  	cardElement.addEventListener('click', flipCard);
+
+  	 document.getElementById('game-board').appendChild(cardElement);
+
+
+
+  }
+};
+
+//call function 
+createBoard (); 
 
